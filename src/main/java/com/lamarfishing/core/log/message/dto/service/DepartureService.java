4ed6f5d8372e264146d6e8fa64ca298e7bc15525
geoKim <kgeo6264@gmail.com>
2025-11-03 20:56:06 +0900

    chore: Add solapi in application.yml using .env

---
+++ src/main/java/com/lamarfishing/core/log/message/dto/service/DepartureService.java
@@ -0,0 +1,50 @@
+package com.lamarfishing.core.log.message.dto.service;
+
+import com.lamarfishing.core.log.message.dto.exception.InvalidDepartureRequest;
+import com.lamarfishing.core.log.message.dto.request.DepartureRequest;
+import com.lamarfishing.core.log.message.dto.response.DepartureResponse;
+import com.lamarfishing.core.reservation.domain.Reservation;
+import com.lamarfishing.core.reservation.repository.ReservationRepository;
+import com.lamarfishing.core.schedule.domain.Schedule;
+import com.lamarfishing.core.schedule.exception.ScheduleInvalidPublicId;
+import com.lamarfishing.core.schedule.exception.ScheduleNotFound;
+import com.lamarfishing.core.schedule.repository.ScheduleRepository;
+import com.lamarfishing.core.user.domain.User;
+import com.lamarfishing.core.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DepartureService {
+    private final ScheduleRepository scheduleRepository;
+    private final ReservationRepository reservationRepository;
+    private final MessageService messageService;
+
+    public DepartureResponse confirmation(String publicId, DepartureRequest departureRequest) {
+        Schedule.Status scheduleStatus = departureRequest.getScheduleStatus();
+        if (scheduleStatus != Schedule.Status.CONFIRMED) {
+            throw new InvalidDepartureRequest();
+        }
+        if (!publicId.startsWith("sch")) {
+            throw new ScheduleInvalidPublicId();
+        }
+
+        Schedule schedule = scheduleRepository.findByPublicId(publicId).orElseThrow(ScheduleNotFound::new);
+        List<Reservation> reservations = reservationRepository.findBySchedule(schedule);
+
+        List<String> phones = new ArrayList<>();
+        for (Reservation reservation : reservations) {
+            User user = reservation.getUser();
+            phones.add(user.getPhone());
+        }
+
+        messageService.sendDepartureConfirmationMessages(phones);
+
+    }
+
+    private
+}
--- src/main/java/com/lamarfishing/core/log/message/dto/service/MessageService.java
+++ src/main/java/com/lamarfishing/core/log/message/dto/service/MessageService.java
@@ -1,48 +1,23 @@
 package com.lamarfishing.core.log.message.dto.service;
 
-import com.lamarfishing.core.log.message.dto.exception.InvalidDepartureRequest;
-import com.lamarfishing.core.log.message.dto.request.DepartureRequest;
-import com.lamarfishing.core.log.message.dto.response.DepartureResponse;
-import com.lamarfishing.core.reservation.domain.Reservation;
-import com.lamarfishing.core.reservation.repository.ReservationRepository;
-import com.lamarfishing.core.schedule.domain.Schedule;
-import com.lamarfishing.core.schedule.exception.ScheduleInvalidPublicId;
-import com.lamarfishing.core.schedule.exception.ScheduleNotFound;
-import com.lamarfishing.core.schedule.repository.ScheduleRepository;
-import com.lamarfishing.core.user.domain.User;
-import com.lamarfishing.core.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.solapi.sdk.message.service.DefaultMessageService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
 public class MessageService {
-    private final ScheduleRepository scheduleRepository;
-    private final ReservationRepository reservationRepository;
-    private final UserRepository userRepository;
+    private DefaultMessageService messageService;
 
-    public DepartureResponse confirmation(String publicId, DepartureRequest departureRequest) {
-        Schedule.Status scheduleStatus = departureRequest.getScheduleStatus();
-        if (scheduleStatus != Schedule.Status.CONFIRMED) {
-            throw new InvalidDepartureRequest();
-        }
-        if (!publicId.startsWith("sch")) {
-            throw new ScheduleInvalidPublicId();
-        }
+    @Value("${solapi.sender}")
+    private String senderNumber;
 
-        Schedule schedule = scheduleRepository.findByPublicId(publicId).orElseThrow(ScheduleNotFound::new);
-        List<Reservation> reservations = reservationRepository.findBySchedule(schedule);
+    @Value("${solapi.api-key}")
+    private String apiKey;
 
-        List<String> phones = new ArrayList<>();
-        for (Reservation reservation : reservations) {
-            User user = reservation.getUser();
-            phones.add(user.getPhone());
-        }
+    @Value("${solapi.api-secret}")
+    private String apiSecret;
 
+    @PostConstruct
+    public void initSolapi() {
+        this.messageService = SolapiClient.INSTANCE.createInstance(apiKey, apiSecret);
     }
-
-    private
 }
