bc59dccce4c3f6d3d1b6fe4dfbd713a6d7ef7942
geoKim <kgeo6264@gmail.com>
2025-11-03 22:08:10 +0900

    feat: Implement confirmation departure controller

---
+++ src/main/java/com/lamarfishing/core/schedule/controller/DepartureController.java
@@ -0,0 +1,25 @@
+package com.lamarfishing.core.schedule.controller;
+
+import com.lamarfishing.core.common.ApiResponse;
+import com.lamarfishing.core.schedule.dto.request.DepartureRequest;
+import com.lamarfishing.core.schedule.dto.response.DepartureResponse;
+import com.lamarfishing.core.schedule.service.DepartureService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/schedules")
+@RequiredArgsConstructor
+public class DepartureController {
+
+    private final DepartureService departureService;
+
+    @PostMapping("/{schedule_public_id}/departure/confirmation")
+    public ResponseEntity<ApiResponse<DepartureResponse>> departureConfirmation(@PathVariable("schedule_public_id") String publicId,
+                                                                                @RequestBody DepartureRequest request){
+        DepartureResponse response = departureService.confirmation(publicId, request);
+
+        return ResponseEntity.ok(ApiResponse.success("출항 확정 메시지를 보냈습니다.",response));
+    }
+}
