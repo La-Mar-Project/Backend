a1597b5032ab5e70c0c08ad09250bb4a93c517d3
geooeg <kgeo6264@gmail.com>
2025-11-07 17:23:08 +0900

    feat: Implement ReservationController

---
+++ src/main/java/com/lamarfishing/core/reservation/controller/ReservationController.java
@@ -0,0 +1,23 @@
+package com.lamarfishing.core.reservation.controller;
+
+import com.lamarfishing.core.common.ApiResponse;
+import com.lamarfishing.core.reservation.dto.response.ReservationDetailResponse;
+import com.lamarfishing.core.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reservation")
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @GetMapping("/{reservationPublicId}")
+    public ResponseEntity<ApiResponse<ReservationDetailResponse>> getReservationDetail(@RequestAttribute(name = "수정필요1") Long userId,
+                                                                                       @PathVariable("reservationPublicId") String publicId){
+        ReservationDetailResponse reservationDetailResponse = reservationService.getReservationDetail(userId, publicId);
+
+        return ResponseEntity.ok(ApiResponse.success("예약 상세 조회에 성공하였습니다.",reservationDetailResponse));
+    }
+}
