3777206e5888d438118c154e1e4f403722773904
geooeg <kgeo6264@gmail.com>
2025-11-10 18:17:26 +0900

    fix: Fix dummy datam in UserDummy

--- src/main/java/com/lamarfishing/core/user/dummy/UserDummy.java
+++ src/main/java/com/lamarfishing/core/user/dummy/UserDummy.java
@@ -19,13 +19,13 @@ public class UserDummy {
         }
 
         userRepository.save(User.create("김승규", "승규천사", User.Grade.BASIC, "01056604120"));
-        userRepository.save(User.create("김준수", "준수악마", User.Grade.ADMIN, "01044966580"));
+        userRepository.save(User.create("김영희", "영희악마", User.Grade.ADMIN, "01012345679"));
         userRepository.save(User.create("김지오", "지오천사", User.Grade.VIP, "01062647243"));
         userRepository.save(User.create("노수현", "수현악마", User.Grade.GUEST, "01055839181"));
         userRepository.save(User.create("원종윤", "종윤천사", User.Grade.BASIC, "01063461851"));
-        userRepository.save(User.create("장창엽", "창엽악마", User.Grade.ADMIN, "01045105619"));
-        userRepository.save(User.create("김철수", "철수천사", User.Grade.VIP, "01099999999"));
-        userRepository.save(User.create("김영희", "영희악마", User.Grade.GUEST, "01088888888"));
+        userRepository.save(User.create("김철수", "철수악마", User.Grade.ADMIN, "01012345678"));
+        userRepository.save(User.create("장창엽", "창엽천사", User.Grade.VIP, "01045105619"));
+        userRepository.save(User.create("김준수", "준수악마", User.Grade.GUEST, "01044966580"));
     }
 
 }
