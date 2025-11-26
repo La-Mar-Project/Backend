60ae8600abb121490c164ef2ab3d81fe4b1bc301
geooeg <kgeo6264@gmail.com>
2025-11-26 14:45:54 +0900

    refactor: Add AuthenticationPrincipal in DepartureController

--- src/main/java/com/lamarfishing/core/schedule/controller/DepartureController.java
+++ src/main/java/com/lamarfishing/core/schedule/controller/DepartureController.java
@@ -5,8 +5,11 @@ import com.lamarfishing.core.schedule.domain.Status;
 import com.lamarfishing.core.schedule.dto.request.DepartureRequest;
 import com.lamarfishing.core.schedule.dto.response.DepartureResponse;
 import com.lamarfishing.core.schedule.service.DepartureService;
+import com.lamarfishing.core.user.dto.command.AuthenticatedUser;
+import com.lamarfishing.core.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,24 +18,17 @@ import org.springframework.web.bind.annotation.*;
 public class DepartureController {
 
     private final DepartureService departureService;
-
+    private final UserService userService;
     /**
      *  출항 확정 메시지 전송
      */
-//    @PostMapping("/{schedulePublicId}/departure/confirmation")
-//    public ResponseEntity<ApiResponse<DepartureResponse>> departureConfirm(@RequestAttribute(name = "수정필요1") Long userId,
-//                                                                           @PathVariable("schedulePublicId") String publicId,
-//                                                                           @RequestBody DepartureRequest request){
-//        DepartureResponse response = departureService.confirmation(userId, publicId, request);
-//
-//        return ResponseEntity.ok(ApiResponse.success("출항 확정 메시지를 보냈습니다.",response));
-//    }
 
     //더미 컨트롤러
     @PostMapping("/{schedulePublicId}/departure/confirmation")
-    public ResponseEntity<ApiResponse<DepartureResponse>> departureConfirm(@PathVariable("schedulePublicId") String publicId,
+    public ResponseEntity<ApiResponse<DepartureResponse>> departureConfirm(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                                                           @PathVariable("schedulePublicId") String publicId,
                                                                            @RequestBody DepartureRequest request){
-        Long userId = 2L;   //관리자
+        Long userId = userService.findUserId(authenticatedUser);
         Status status = request.getScheduleStatus();
         DepartureResponse response = departureService.confirmation(userId, publicId, status);
 
@@ -42,19 +38,12 @@ public class DepartureController {
     /**
      * 출항 취소
      */
-//    @PostMapping("/{schedulePublicId}/departure/cancel")
-//    public ResponseEntity<ApiResponse<DepartureResponse>> departureCancel(@RequestAttribute(name = "수정필요1") Long userId,
-//                                                                          @PathVariable("schedulePublicId") String publicId,
-//                                                                          @RequestBody DepartureRequest request){
-//        DepartureResponse response = departureService.cancel(userId, publicId, request);
-//
-//        return ResponseEntity.ok(ApiResponse.success("출항 취소 메시지를 보냈습니다.",response));
-//    }
 
     @PostMapping("/{schedulePublicId}/departure/cancel")
-    public ResponseEntity<ApiResponse<DepartureResponse>> departureCancel(@PathVariable("schedulePublicId") String publicId,
+    public ResponseEntity<ApiResponse<DepartureResponse>> departureCancel(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                                                          @PathVariable("schedulePublicId") String publicId,
                                                                           @RequestBody DepartureRequest request){
-        Long userId = 2L;
+        Long userId = userService.findUserId(authenticatedUser);
         Status status = request.getScheduleStatus();
         DepartureResponse response = departureService.cancel(userId, publicId, status);
 
@@ -64,19 +53,12 @@ public class DepartureController {
     /**
      * 출항 연기
      */
-//    @PostMapping("/{schedulePublicId}/departure/delay")
-//    public ResponseEntity<ApiResponse<DepartureResponse>> departureDelay(@RequestAttribute(name = "수정필요1") Long userId,
-//                                                                         @PathVariable("schedulePublicId") String publicId,
-//                                                                         @RequestBody DepartureRequest request){
-//        DepartureResponse response = departureService.cancel(userId, publicId, request);
-//
-//        return ResponseEntity.ok(ApiResponse.success("출항 보류 메시지를 보냈습니다.",response));
-//    }
 
     @PostMapping("/{schedulePublicId}/departure/delay")
-    public ResponseEntity<ApiResponse<DepartureResponse>> departureDelay(@PathVariable("schedulePublicId") String publicId,
+    public ResponseEntity<ApiResponse<DepartureResponse>> departureDelay(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                                                         @PathVariable("schedulePublicId") String publicId,
                                                                          @RequestBody DepartureRequest request){
-        Long userId = 2L;
+        Long userId = userService.findUserId(authenticatedUser);
         Status status = request.getScheduleStatus();
         DepartureResponse response = departureService.delay(userId, publicId, status);
 
